2ae379f7af2fcad98aeac6006f8c5f5b35c8d297
Tate Johnson <tate@tatey.com>
2019-11-27 09:07:23 +1000

    Prefix echo with where it comes from.

--- datadog/prerun.sh
+++ datadog/prerun.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo "Tags dyno, dynotype, and appname have been disabled"
+echo "[Datadog Prerun] Tags dyno, dynotype, and appname have been disabled"
 sed -i -e 's/^  - dyno:.*$/# &/' "$DATADOG_CONF"
 sed -i -e 's/^  - dynotype:.*$/# &/' "$DATADOG_CONF"
 sed -i -e 's/^  - appname:.*$/# &/' "$DATADOG_CONF"
