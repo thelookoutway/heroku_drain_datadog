12d87acf29747a5917ff19a03fe1197d6e090d78
Lang Sharpe <lang.sharpe@fivegoodfriends.com.au>
2022-10-04 11:24:51 +1000

    Rename source to dyno

--- spec/heroku_drain_datadog/http/router_spec.rb
+++ spec/heroku_drain_datadog/http/router_spec.rb
@@ -93,11 +93,11 @@ RSpec.describe HerokuDrainDatadog::HTTP::Router do
           end
 
           it "sends a histogram for connect" do
-            expect(output[0]).to eq("heroku.router.connect:0.0|h|#source:web.1,dynotype:web,method:GET,status:304")
+            expect(output[0]).to eq("heroku.router.connect:0.0|h|#dyno:web.1,dynotype:web,method:GET,status:304")
           end
 
           it "sends a histogram for service" do
-            expect(output[1]).to eq("heroku.router.service:2.0|h|#source:web.1,dynotype:web,method:GET,status:304")
+            expect(output[1]).to eq("heroku.router.service:2.0|h|#dyno:web.1,dynotype:web,method:GET,status:304")
           end
 
           it "increments drain" do
@@ -120,23 +120,23 @@ RSpec.describe HerokuDrainDatadog::HTTP::Router do
           end
 
           it "sends a gauge for memory_cache" do
-            expect(output[0]).to eq("heroku.dyno.memory_cache:3.63|g|#source:web.1,dynotype:web")
+            expect(output[0]).to eq("heroku.dyno.memory_cache:3.63|g|#dyno:web.1,dynotype:web")
           end
 
           it "sends a gauge for memory_quota" do
-            expect(output[1]).to eq("heroku.dyno.memory_quota:512.0|g|#source:web.1,dynotype:web")
+            expect(output[1]).to eq("heroku.dyno.memory_quota:512.0|g|#dyno:web.1,dynotype:web")
           end
 
           it "sends a gauge for memory_rss" do
-            expect(output[2]).to eq("heroku.dyno.memory_rss:139.79|g|#source:web.1,dynotype:web")
+            expect(output[2]).to eq("heroku.dyno.memory_rss:139.79|g|#dyno:web.1,dynotype:web")
           end
 
           it "sends a gauge for memory_swap" do
-            expect(output[3]).to eq("heroku.dyno.memory_swap:11.43|g|#source:web.1,dynotype:web")
+            expect(output[3]).to eq("heroku.dyno.memory_swap:11.43|g|#dyno:web.1,dynotype:web")
           end
 
           it "sends a gauge for memory_quota" do
-            expect(output[4]).to eq("heroku.dyno.memory_total:154.85|g|#source:web.1,dynotype:web")
+            expect(output[4]).to eq("heroku.dyno.memory_total:154.85|g|#dyno:web.1,dynotype:web")
           end
 
           it "increments drain" do
@@ -284,14 +284,14 @@ RSpec.describe HerokuDrainDatadog::HTTP::Router do
           it "can be a single tag" do
             with_env("DRAIN_TAGS_FOR_abc123", "service:myapp") do
               post "/logs", %q{338 <158>1 2016-08-20T02:15:10.862264+00:00 host heroku router - at=info method=GET path="/assets/admin-62f13e9f7cb78a2b3e436feaedd07fd67b74cce818f3bb7cfdab1e1c05dc2f89.css" host=app.fivegoodfriends.com.au request_id=bef7f609-eceb-4684-90ce-c249e6843112 fwd="58.6.203.42,54.239.202.42" dyno=web.1 connect=0ms service=2ms status=304 bytes=112}
-              expect(output[0]).to eq("heroku.router.connect:0.0|h|#service:myapp,source:web.1,dynotype:web,method:GET,status:304")
+              expect(output[0]).to eq("heroku.router.connect:0.0|h|#service:myapp,dyno:web.1,dynotype:web,method:GET,status:304")
             end
           end
 
           it "can be many tags" do
             with_env("DRAIN_TAGS_FOR_abc123", "env:production,service:myapp") do
               post "/logs", %q{338 <158>1 2016-08-20T02:15:10.862264+00:00 host heroku router - at=info method=GET path="/assets/admin-62f13e9f7cb78a2b3e436feaedd07fd67b74cce818f3bb7cfdab1e1c05dc2f89.css" host=app.fivegoodfriends.com.au request_id=bef7f609-eceb-4684-90ce-c249e6843112 fwd="58.6.203.42,54.239.202.42" dyno=web.1 connect=0ms service=2ms status=304 bytes=112}
-              expect(output[0]).to eq("heroku.router.connect:0.0|h|#env:production,service:myapp,source:web.1,dynotype:web,method:GET,status:304")
+              expect(output[0]).to eq("heroku.router.connect:0.0|h|#env:production,service:myapp,dyno:web.1,dynotype:web,method:GET,status:304")
             end
           end
 
